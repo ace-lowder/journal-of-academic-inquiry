707f4444279d46534a8efe2fbe20bf96ba6186e7
Ace <ace.lowder@gmail.com>
2024-08-26 16:47:23 -0700

    Add spacing to bottom of homepage

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7c1506f5cf3807eb"; }
+/******/ 		__webpack_require__.h = function() { return "3198fef9aad50dc2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import FeaturedArticle from '../components/sections/FeaturedArticle';
 import CurrentIssue from '../components/sections/CurrentIssue';
 
 const Home: React.FC = () => (
-  <div className="max-w-[1280px] mx-auto flex flex-col gap-12">
+  <div className="max-w-[1280px] mx-auto flex flex-col gap-12 pb-24">
     <FeaturedArticle />
     <CurrentIssue />
   </div>
