9a9c8ed70d69d0c14210ae946246439bd78e9f79
Ace <ace.lowder@gmail.com>
2024-08-26 14:13:12 -0700

    Add dropdown menu to mobile navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9c4c5316ac815d05"; }
+/******/ 		__webpack_require__.h = function() { return "034c8dcaa8107f56"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/layout/MobileMenu.tsx
+++ src/components/layout/MobileMenu.tsx
@@ -1,18 +1,54 @@
 import Link from 'next/link';
-import { MdClose } from 'react-icons/md'; // Importing the more angular close icon
+import { useState } from 'react';
+import { MdClose } from 'react-icons/md';
+import { FaChevronDown } from 'react-icons/fa';
 
 interface MobileMenuProps {
   closeMenu: () => void;
 }
 
 const MobileMenu: React.FC<MobileMenuProps> = ({ closeMenu }) => {
+  const [openDropdown, setOpenDropdown] = useState<string | null>(null);
+
   const menuItems = [
-    { text: 'Home', href: '/' },
-    { text: 'About', href: '/about' },
-    { text: 'Coaching', href: '/coaching' },
-    { text: 'Submit', href: '/submit' },
+    {
+      text: 'Home',
+      href: '/',
+    },
+    {
+      text: 'About',
+      href: '/about',
+      subItems: [
+        { text: 'Mission Statement', href: '/about/mission' },
+        { text: 'Focus Areas', href: '/about/focus' },
+      ],
+    },
+    {
+      text: 'Services',
+      href: '/services',
+      subItems: [
+        { text: 'Coaching', href: '/services/coaching' },
+        { text: 'Online Review', href: '/services/review' },
+      ],
+    },
+    {
+      text: 'Submit',
+      href: '/submit',
+      subItems: [
+        { text: 'Requirements', href: '/submit/requirements' },
+        { text: 'How to Submit', href: '/submit/how-to' },
+      ],
+    },
     { text: 'Contact', href: '/contact' },
   ];
+
+  const handleDropdownToggle = (menuKey: string) => {
+    if (openDropdown === menuKey) {
+      setOpenDropdown(null);
+    } else {
+      setOpenDropdown(menuKey);
+    }
+  };
 
   return (
     <nav className="fixed inset-0 bg-white z-50 flex flex-col border border-black">
@@ -24,19 +60,43 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ closeMenu }) => {
         <MdClose size={24} />
       </button>
 
-      <ul className="mt-16 w-full px-8">
-        {menuItems.map(({ text, href }, index) => (
-          <li className="w-full" key={text}>
-            <Link href={href} passHref>
-              <div
-                onClick={closeMenu}
-                className={`block py-6 text-center cursor-pointer ${
-                  index < menuItems.length - 1 ? 'border-b border-gray-300' : ''
-                }`}
-              >
-                {text}
-              </div>
-            </Link>
+      <ul className="mt-16 w-full px-4">
+        {menuItems.map(({ text, href, subItems }) => (
+          <li key={text}>
+            <div className="flex justify-between items-center cursor-pointer border-b border-gray-300 ">
+              <Link href={href} passHref className="w-full">
+                <div
+                  onClick={closeMenu}
+                  className="text-gray-700 w-full pl-4 py-8"
+                >
+                  {text}
+                </div>
+              </Link>
+              {subItems && (
+                <button
+                  className="text-gray-700 focus:outline-none py-9"
+                  onClick={() => handleDropdownToggle(text)}
+                >
+                  <FaChevronDown className="text-sm text-gray-500 ml-12 mr-6" />
+                </button>
+              )}
+            </div>
+            {subItems && openDropdown === text && (
+              <ul>
+                {subItems.map(({ text, href }) => (
+                  <li key={text} className="w-full">
+                    <Link href={href} passHref>
+                      <div
+                        onClick={closeMenu}
+                        className="px-6 py-8 text-center text-gray-700 border-b border-gray-300"
+                      >
+                        {text}
+                      </div>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            )}
           </li>
         ))}
       </ul>
